3a89c48601d113973ace97f85585be26490458f5
aris <arisai.astra.co.id>
2024-05-12 21:53:35 +0700

    js

---
+++ js/ch1all.js
@@ -0,0 +1,163 @@
+// Cetak "Hello World" ke console
+console.log("Hello World");
+
+// Cetak "Ninja Ken" ke console
+console.log("Ninja Ken");
+
+// Ubah baris di bawah menjadi komentar
+// console.log("Ubah baris ini menjadi komentar");
+
+// ###################################
+// Cetak hasil 5 tambah 3
+console.log(5 + 3);
+
+// Cetak hasil 20 kurang 8
+console.log(20 - 8);
+
+// Cetak string "4 + 5"
+console.log("4 + 5");
+
+
+
+
+// Cetak hasil 8 kali 4 di console.
+console.log(8 * 4);
+
+// Cetak hasil 24 bagi 4 di console.
+console.log(24 / 4);
+
+// Cetak sisa setelah membagi 7 dengan 2 di console.
+console.log(7 % 2);
+
+
+
+
+// Cetak kombinasi dari string "Guru " dan "Domba"
+console.log("Guru " + "Domba");
+
+// Cetak kombinasi dari "20" dan "15" (dan jadikan sebagai string)
+console.log("20"+"15");
+
+
+
+
+
+// Deklarasikan nama variable dengan nilai string "Ninja Ken"
+let name = "Ninja Ken";
+
+// Cetakan nilai nama variable
+console.log(name);
+
+
+// Deklarasikan variable length
+let length=5;
+
+// Cetak nilai variable length
+console.log(length);
+
+// Gunakan variable length untuk mencetak hasil area lingkaran
+console.log(3*length*length);
+
+
+
+
+// let name = "Ninja Ken";
+console.log(name);
+name = "Birdie";
+console.log(name);
+
+
+
+
+
+let number = 7;
+console.log(number);
+
+// Tambahkan 3 ke nilai variable number
+number+=3;
+
+console.log(number);
+
+// Bagi nilai variable number dengan 2
+number= number/2;
+
+console.log(number);
+
+
+
+
+
+// Deklarasikan constant language
+const  language= "Prancis";
+
+// Cetak nilai constant language
+console.log(language);
+
+// Gunakan constant language untuk mencetak "Saya bisa berbicara bahasa ____"
+console.log("Saya bisa berbicara bahasa "+language);
+
+
+
+
+
+
+
+
+// const name = "Ninja Ken";
+const age = 14;
+
+// Cetak string "Nama saya adalah ____"
+console.log("Nama saya adalah "+ name);
+
+
+
+// Cetak string "Hari ini saya berusia ____ tahun"
+
+console.log("Hari ini saya berusia "+age+" tahun ");
+
+
+
+
+
+// const name = "Ninja Ken";
+// const age = 14;
+
+// Cetak string "Nama saya adalah ____"
+console.log(`Nama saya adalah ${name}`);
+
+
+
+// Cetak string "Hari ini saya berusia ____ tahun"
+
+console.log(`Hari ini saya berusia ${age} tahun`);
+
+     
+
+
+
+
+
+
+const level = 12;
+
+// Tambahkan pernyataan if dengan kondisi: level > 10
+if (level>10) {
+  console.log("Level Anda lebih tinggi dari 10");
+}
+
+
+
+// const age = 24;
+
+// Cetak hasil dari age >= 18
+console.log(age >= 18);
+
+// Cetak hasil dari age < 18
+console.log(age < 18);
+
+/* Ketika nilai dari age lebih besar atau sama dengan 18
+dan cetak "Saya berusia lebih dari 18 tahun" */
+if (age >= 18) {
+    console.log("Saya berusia lebih dari 18 tahun");
+}
+
